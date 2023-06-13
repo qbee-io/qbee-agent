8a8cb70e19f20a361515d55e59661f5d260f77d6
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2023-06-13 09:57:16 +0200

    Set global umask to 0077

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
 
 	"github.com/qbee-io/qbee-agent/app/cmd"
 )
+
+var defaultUmask int = 0077
+
+func init() {
+	// set global umask
+	syscall.Umask(defaultUmask)
+}
 
 func main() {
 	if err := cmd.Main.Execute(os.Args[1:], nil); err != nil {
