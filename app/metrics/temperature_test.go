28545c0859612a949254369417cfb9391f89d7cd
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2024-02-22 11:53:16 +0100

    Collect only cpu temp metrics

---
+++ app/metrics/temperature_test.go
@@ -0,0 +1,56 @@
+// Copyright 2024 qbee.io
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package metrics
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestCollectTemperature(t *testing.T) {
+
+	files, _ := getHwMonFiles()
+	if len(files) == 0 {
+		files, _ = getThermalZoneFiles()
+		if len(files) == 0 {
+			// No temperature files found, skip test. CI uses vms and they don't have temperature files.
+			t.Skip("no temperature files found")
+		}
+	}
+
+	metrics, err := CollectTemperature()
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	fmt.Printf("metrics: %v\n", metrics)
+	for _, metric := range metrics {
+		if metric.Label != Temperature {
+			t.Errorf("unexpected label: %v", metric.Label)
+		}
+		if metric.Values.Temperature <= 0 {
+			t.Errorf("unexpected temperature: %v", metric.Values.Temperature)
+		}
+	}
+
+	b, err := json.MarshalIndent(metrics, "", "  ")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	t.Fatalf("metrics: %s", string(b))
+}
