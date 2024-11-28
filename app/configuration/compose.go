b686eb567daf753e84d2b49492b9ed55360e3441
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2024-11-28 11:31:04 +0100

    Skip restart

--- app/configuration/compose.go
+++ app/configuration/compose.go
@@ -8,11 +8,16 @@ type Compose struct {
 	// File to the docker-compose file.
 	File string `json:"file"`
 
-	// ComposeContent is the content any build context.
+	// ComposeContent is the content any build context (tarball) that is needed for the compose file.
+	// NB: It is not recommend using build context in production environments as it will not create
+	// immutable deployments. Use it only for development purposes.
 	Context string `json:"context,omitempty"`
 
 	// PreCondition is a shell command that needs to be true before starting the container.
 	PreCondition string `json:"pre_condition,omitempty"`
+
+	// SkipRestart skips the restart of the container.
+	SkipRestart bool `json:"skip_restart,omitempty"`
 
 	// Parameters are the parameters that can be used in the compose file.
 	Parameters []TemplateParameter `json:"parameters,omitempty"`
