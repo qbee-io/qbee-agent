2fd81f190b889ce468de0d0127fc7bc0bdf9c472
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2025-06-03 16:04:28 +0200

    windows specific default paths

---
+++ app/cmd/paths_unix.go
@@ -0,0 +1,8 @@
+//go:build unix
+
+package cmd
+
+const (
+	defaultConfigDir = "/etc/qbee"
+	defaultStateDir  = "/var/lib/qbee"
+)
---
+++ app/cmd/paths_windows.go
@@ -0,0 +1,8 @@
+//go:build windows
+
+package cmd
+
+const (
+	defaultConfigDir = `C:\Program Files\qbee\etc`
+	defaultStateDir  = `C:\Program Files\qbee\var`
+)
