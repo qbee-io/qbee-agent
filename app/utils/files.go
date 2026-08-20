252c653cdca5eb6b283ee93240a5c8f7ff183e92
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2026-08-20 09:09:44 +0200

    init no-block

--- app/utils/files.go
+++ app/utils/files.go
@@ -1,6 +1,11 @@
 package utils
 
-import "os"
+import (
+	"context"
+	"io"
+	"os"
+	"sync/atomic"
+)
 
 // WriteFileSync writes data to a file named by filename and syncs to disk.
 func WriteFileSync(name string, data []byte, perm os.FileMode) error {
@@ -27,4 +32,80 @@ func WriteFileSync(name string, data []byte, perm os.FileMode) error {
 	}
 
 	return err
+}
+
+// goroutineCount tracks the number of active goroutines
+var goroutineCount int64
+
+const maxGoroutines = 500
+
+func ReadFileContext(ctx context.Context, filePath string) ([]byte, error) {
+	atomic.AddInt64(&goroutineCount, 1)
+	defer atomic.AddInt64(&goroutineCount, -1)
+
+	ch := make(chan []byte, 1)
+	errCh := make(chan error, 1)
+
+	go func() {
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		ch <- data
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case err := <-errCh:
+		return nil, err
+	case data := <-ch:
+		return data, nil
+	}
+}
+
+// OpenFileContext opens a file with context cancellation support.
+func OpenFileContext(ctx context.Context, filePath string) (io.Reader, error) {
+	atomic.AddInt64(&goroutineCount, 1)
+
+	ch := make(chan *os.File, 1)
+	errCh := make(chan error, 1)
+
+	go func() {
+		defer atomic.AddInt64(&goroutineCount, -1)
+		f, err := os.Open(filePath)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		ch <- f
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case err := <-errCh:
+		return nil, err
+	case f := <-ch:
+		return &contextReader{
+			ctx: ctx,
+			f:   f,
+		}, nil
+	}
+}
+
+type contextReader struct {
+	ctx context.Context
+	f   *os.File
+}
+
+func (cr *contextReader) Read(p []byte) (n int, err error) {
+	select {
+	case <-cr.ctx.Done():
+		cr.f.Close()
+		return 0, cr.ctx.Err()
+	default:
+	}
+	return cr.f.Read(p)
 }
